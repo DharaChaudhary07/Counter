5fd43955b343f645341a2c79fcf761625cb614be
DharaChaudhary07 <dharachaudhary683@gmail.com>
2024-08-12 13:36:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Count from  './Component/Counter/Counter.jsx'
+
+function App() {
+
+  <Count />
+  
+}
+
+export default App
